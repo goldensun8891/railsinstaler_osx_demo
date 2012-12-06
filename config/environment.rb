4b2e3e36a0c78b1602c4ee7696bda1627e252569
Kevin Li <goldensun8891@gmail.com>
2012-12-06 16:03:09 -0500

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+8# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+RailsinstallerOsxDemo::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerOsxDemo::Application.config.secret_token = '7179670bda99a75e868dbdef77406370aedbcf448e3441bdad2ddaa6f58dd9c019ef53ead6db59f8bd20394429c75a2f47d80eb1a8dde2099e777b4d0b8888b4'
